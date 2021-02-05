947b61fac259719a9425c131c048e0b06c466317
Shane Harper <cdtharper@gmail.com>
2021-02-04 20:51:04 -0500

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,74 +1,26 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import TermsOfService from './components/TermsOfService';
-import Report from './report';
-
-import { renderButton, checkSignedIn } from "./utils";
 
 import logo from './logo.svg';
 import './App.css';
 
-// class App extends Component {
+class App extends Component {
   
-//   render() {
+  render() {
 
-//     return (
-//       <Router>
-//         <div className="App">
-//             {!isSignedIn ? (
-//             <div id="signin-button"></div>
-//           ) : (
-//             <div>Coming soon...</div>
-//           )}
-//           <Switch>
-//             <Route exact path="/" component={Home} />
-//             <Route path="/privacy-policy" component={PrivacyPolicy} />
-//             <Route path="/privacy-policy" component={TermsOfService} />
-//           </Switch>
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
-
-function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
-    const updateSignin = (signedIn) => { //(3)
-      setIsSignedIn(signedIn);
-      if (!signedIn) {
-        renderButton();
-      }
-    };
-  
-    const init = () => { //(2)
-      checkSignedIn()
-        .then((signedIn) => {
-          updateSignin(signedIn);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    };
-  
-    useEffect(() => {
-      window.gapi.load("auth2", init); //(1)
-    });
-
-  return(
-    <div className="App">
-      {!isSignedIn ? (
-        <div id="signin-button"></div>
-          ) : (
-          <Report />
-      )}
-    </div>
-  );
-
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 function mapStateToProps(state) {
